4f1537cc3431276bfe7a8b4573eba774f157a25c
uniquessatacado <ussloja@gmail.com>
2025-12-15 03:03:48 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "../App";
 
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
+
